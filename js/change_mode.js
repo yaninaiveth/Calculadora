bbd9f0c2f9287bbad821311d375ba451090bb231
Silvio Molina <molinahuel44@gmail.com>
2021-11-06 22:27:43 -0300

    adding things tha maybe work or not

--- js/change_mode.js
+++ js/change_mode.js
@@ -1,15 +1,17 @@
 var available_op = ['sumar','restar','multiplicar','dividir', 'igual']
-var new_ops = ['sen','cos','arcos','arcosen','tangente']
 
-var MODE_COLOR = ''
+var new_ops_blue = ['sen','cos','tangente','exp','raiz']
+var new_ops_red = ['arcsen','arcocos', 'arctan', 'ln', 'pot']
+
+var MODE_COLOR = 'blue'
 
 var colorMode = color => {
-	if(color === 'red'){
+	if(MODE_COLOR === 'blue'){
 		MODE_COLOR = 'red'
-	}else if(color === 'blue'){
+	}else if(MODE_COLOR === 'red'){
 		MODE_COLOR = 'blue'
 	}else{
-		return `That color mode is not available`
+		return `It should has an initial color`
 	}
 }
 
@@ -21,16 +23,21 @@ var changeMode = e => {
 	
 		if(available_op.includes(btn.value)){
 
-			btn.value = new_ops[i]
-			btn.innerHTML = new_ops[i]
+			if(MODE_COLOR === 'red'){
+				btn.value = new_ops_red[i]
+				btn.innerHTML = new_ops_red[i]
+			}else if (MODE_COLOR === 'blue'){
+				btn.value = new_ops_blue[i]
+				btn.innerHTML = new_ops_blue[i]
+			}
 			i++
 		}		
 	})
 }
 
 
-var MODE = document.getElementById('mode_red').addEventListener('click', _ => colorMode('red'))
-var MODE = document.getElementById('blue_red').addEventListener('click', _ => colorMode('blue'))
+var MODE   = document.getElementById('mode_color')
+var CHANGE = document.getElementById('change')
 
-var change = document.getElementById('change')
-change.addEventListener('click', changeMode)
+MODE.addEventListener('click', _ => colorMode())
+CHANGE.addEventListener('click', changeMode)
